e8711c8e1e63705150d0a8f0fc78616e8c67fefc
Pierre <pierrelidem@gmail.com>
2023-03-01 15:24:10 +0100

    html/css

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boutique</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <a href="panier.php" class="link">Panier <span>8</span> </a>
+    <section class="products_list">
+        <form action="" class="product">
+            <div class="image_product">
+                <img src="img/img1.png" alt="">
+            </div>
+            <div class="content">
+                <h4 class="name">Poke ball</h4>
+                <h2 class="price">12€</h2>
+                <a href="#" class="id_product">Ajouter au panier</a>
+            </div>
+        </form>
+        <form action="" class="product">
+            <div class="image_product">
+                <img src="img/img1.png" alt="">
+            </div>
+            <div class="content">
+                <h4 class="name">Poke ball</h4>
+                <h2 class="price">12€</h2>
+                <a href="#" class="id_product">Ajouter au panier</a>
+            </div>
+        </form>
+        <form action="" class="product">
+            <div class="image_product">
+                <img src="img/img1.png" alt="">
+            </div>
+            <div class="content">
+                <h4 class="name">Poke ball</h4>
+                <h2 class="price">12€</h2>
+                <a href="#" class="id_product">Ajouter au panier</a>
+            </div>
+        </form>
+    </section>
+
+</body>
+
+</html>
---
+++ panier.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panier</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="panier">
+    <a href="index.php" class="link">Boutique</a>
+    <section>
+        <table>
+            <tr>
+                <th></th>
+                <th>Nom</th>
+                <th>Prix</th>
+                <th>Quantité</th>
+                <th>Action</th>
+            </tr>
+
+            <tr>
+                <td><img src="img/img1.png" alt=""> </td>
+                <td>Meat</td>
+                <td>12€</td>
+                <td>5</td>
+                <td> <img src="img/delete.png" alt=""></td>
+            </tr>
+
+            <tr class="total">
+
+                <th>Total: 25€</th>
+            </tr>
+        </table>
+    </section>
+</body>
+
+</html>
